9c82bbf1fe51b7f00407a57c8833caae99991adc
Gustav <gustavbeier@gmail.com>
2024-11-07 00:20:33 -0300

    Feat/instalacao componentes (#5)
    * feat: adjust architecture (#1)
    
    * feat: database + get database + adjust infra
    
    * feat: database + get database + adjust infra
    
    * WIP
    
    * WIP
    
    * Ajuste na filtragem de flags
    
    ---------
    
    Co-authored-by: Naan <88684581+NaanAndrade@users.noreply.github.com>

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+import { AppSidebar } from "@/components/app-sidebar";
+
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  );
+}
