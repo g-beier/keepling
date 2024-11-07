d950c4bc2cd4cde8fdc16637c131fb584802f9a1
Naan Andrade <naan.andrade@caf.io>
2024-11-07 00:11:22 -0300

    fix: adjust ids and flags api

--- pages/api/v1/flags/index.ts
+++ pages/api/v1/flags/index.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import path from 'path';
 import database from '../../../../infra/database.json';
 
-function getFlags(tipo: string) {
+function getTags(tipo: string) {
   const tags = database[0].atividade.tags;
-  return tags.filter((flag) => flag.tipo === tipo);
+
+  if (tipo === 'all') return tags;
+
+  return tags.filter((tag: any) => {tag.tipo === tipo});;
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -17,12 +18,12 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { tipo } = req.query;
 
   if (!tipo) {
-    return res.status(400).json({ error: 'Flag query parameter is required' });
+    return res.status(400).json({ error: 'tag query parameter is required' });
   }
   
   try {
-    const flags = getFlags(tipo as string);
-    return res.status(200).json({ flags });
+    const tags = getTags(tipo as string);
+    return res.status(200).json(tags);
   } catch (error) {
     return res.status(500).json({ error: 'Internal Server Error'});
   }
